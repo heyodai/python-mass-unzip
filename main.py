f8b4b3a33f5559f938bc4ad58d74c5ede84732b1
Odai Athamneh <heyodai@gmail.com>
2023-06-07 18:59:46 -0500

    Add working script

---
+++ main.py
@@ -0,0 +1,41 @@
+import os
+import argparse
+import logging
+import zipfile
+import time
+
+epoch_time = int(time.time())
+logging.basicConfig(filename=f'logs/{epoch_time}.log', level=logging.INFO,
+                    format='%(asctime)s - %(levelname)s - %(message)s')
+
+def unzip_file(full_path, directory):
+    try:
+        with zipfile.ZipFile(full_path, 'r') as zip_ref:
+            logging.info(f'Unzipping file {full_path}')
+            zip_ref.extractall(directory)
+        logging.info(f'Successfully unzipped file {full_path}')
+    except Exception as e:
+        logging.error(f'Failed to unzip file {full_path}, error: {str(e)}')
+
+def delete_file(full_path):
+    try:
+        os.remove(full_path)
+        logging.info(f'Successfully deleted zip file {full_path}')
+    except Exception as e:
+        logging.error(f'Failed to delete file {full_path}, error: {str(e)}')
+
+def main():
+    parser = argparse.ArgumentParser(description='Unzip and delete zip files in a directory.')
+    parser.add_argument('dir', type=str, help='The directory to process')
+    
+    args = parser.parse_args()
+    directory = args.dir
+
+    for filename in os.listdir(directory):
+        if filename.endswith(".zip"):
+            full_path = os.path.join(directory, filename)
+            unzip_file(full_path, directory)
+            delete_file(full_path)
+
+if __name__ == "__main__":
+    main()
